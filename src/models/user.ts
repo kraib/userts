0a4a0d140db40872ebf952a36612a84acd844002
Kraiba Semakula <krabz01@gmail.com>
2018-05-09 18:02:29 +0300

    add lint

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,5 +2,5 @@ import { Document } from "mongoose";
 import { IUser } from "../interfaces/user";
 
 export interface IUserModel extends IUser, Document {
-  //custom methods for your model would be defined here
+  // custom methods for your model would be defined here
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response, Router } from "express";
 import { BaseRoute } from "./route";
 export class IndexRoute extends BaseRoute {
   public static create(router: Router) {
-    console.log("[IndexRoute::create] Creating index route.");
     router.get("/", (req: Request, res: Response, next: NextFunction) => {
       new IndexRoute().index(req, res, next);
     });
@@ -13,8 +12,8 @@ export class IndexRoute extends BaseRoute {
 
   public index(req: Request, res: Response, next: NextFunction) {
     this.title = "Welcome";
-    let options: Object = {
-      message: "Welcome to the Tour of Heros"
+    const options: Object = {
+      message: "Welcome"
     };
     this.render(req, res, "index", options);
   }
